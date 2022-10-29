df064b8ec7ab4a430c28a21396e44d2acf003d13
mukswilly <napsterphantom@gmail.com>
2022-10-29 16:02:35 +0300

    edit copyright

--- config.go
+++ config.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, Mukama Willy Fortunate.
+ * Copyright (c) 2022, Mukama Willy Fortunate <napsterphantom@gmail.com>.
  * All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
--- run.go
+++ run.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, Mukama Willy Fortunate.
+ * Copyright (c) 2022, Mukama Willy Fortunate <napsterphantom@gmail.com>.
  * All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
- 
+
 package udpgw
 
 import (
